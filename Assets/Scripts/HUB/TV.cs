07c885ab3056cca0b32ac4f51c0f63f23240b805
Victor <61842979+Dirtcake@users.noreply.github.com>
2021-05-04 20:33:37 -0300

    TvFofa
    Co-Authored-By: ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>

--- Assets/Scripts/HUB/TV.cs
+++ Assets/Scripts/HUB/TV.cs
@@ -1,76 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Video;
 
 public class TV : MonoBehaviour, IInteragivel
 {
-    #region [ Public ]
-
+    public MeshRenderer mr;
     public Material highLightMaterial;
     public Material defaultMaterial;
-    public Material memoriaMaterial;
 
     [Space(20)]
+    public UnityEvent onClick;
 
-    public MeshRenderer mr;
-
-    public VideoClip chiado;
-    public VideoClip memoria;
-
-    public GameObject tela;
-    public GameObject telaMemoria;
-
-    #endregion
-
-    #region [ Private ]
-
-    private Modulos adicionarEspaco;
-
-    #endregion
-
-    public void Start()
-    {
-        adicionarEspaco = GetComponent<Modulos>();
-    }
     public void DownLight()
     {
-        mr.material = defaultMaterial;   
+        mr.material = defaultMaterial;
     }
 
     public void HighLight()
-    {      
-            mr.material = highLightMaterial;
-            PlayVideo(chiado, tela);    
+    {
+        mr.material = highLightMaterial;
     }
 
     public void Interaction()
     {
-        ScreenInvert();
-        PlayVideo(memoria,telaMemoria);
-        //TODO animacao aproximar da tv 
-    }
-
-    public void PosInteraction()
-    {
-        adicionarEspaco.AumentarHUB();
-        ScreenInvert();
-        
-    }
-    public void ScreenInvert()
-    {
-        var actualState = tela.activeSelf;
-
-        telaMemoria.SetActive(actualState);
-        tela.SetActive(!actualState);
-        //TODO mudanca de material em vez de telas realmente
-    }
-    public void PlayVideo(VideoClip video, GameObject tela)
-    {
-        tela.GetComponent<VideoPlayer>().clip = video;
-        tela.GetComponent<VideoPlayer>().Play();
-
-        if (telaMemoria.GetComponent<VideoPlayer>().isPaused)
-            PosInteraction();
+        onClick.Invoke();
     }
 }
